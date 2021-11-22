b5e737ff8e851d3639c3d286c702eb1592616720
Evan <elafleur@pdx.edu>
2021-11-21 23:52:27 -0800

    Moved menu to CRT deco and tweaked a few things with sizing

--- src/main.js
+++ src/main.js
@@ -12,3 +12,22 @@ setInterval(() => {
         cursor = true;
     }
 }, speed);
+
+// document.addEventListener('DOMContentLoaded', function() {
+ 
+//     window.addEventListener('scroll', stickyBar);
+  
+//     // window.onscroll = function() {stickyBar()};
+//     var navbar = document.getElementById("navbar");
+//     var sticky = navbar.offsetTop;
+
+//     function stickyBar() {
+//         if (window.pageYOffset >= sticky) {
+//             navbar.classList.add("sticky");
+//         }
+//         else 
+//         {
+//             navbar.classList.remove("sticky");
+//         }
+//     }
+// })
