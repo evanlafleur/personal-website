d69b13db6adf6f9fc4db7f670f1939dee8b189fd
Evan <elafleur@pdx.edu>
2021-11-25 23:41:04 -0800

    Added Contact form

--- src/main.js
+++ src/main.js
@@ -13,6 +13,27 @@ setInterval(() => {
     }
 }, speed);
 
+function validate() {
+    var name = document.forms["contactMe"]["name"];
+    var msg = document.forms["contactMe"]["message"];
+
+    //If name is empty
+    if (name.value == "") {
+        document.getElementById('errorname').innerHTML="Enter a valid name";
+        return false;
+    } else {
+        document.getElementById('errorname').innerHTML="";
+    }
+
+    //If message is empty
+    if (msg.value == "") {
+        document.getElementById('errormsg').innerHTML="Please enter a message to send";
+        return false;
+    } else {
+        document.getElementById('errormsg').innerHTML="";
+    }
+}
+
 // document.addEventListener('DOMContentLoaded', function() {
  
 //     window.addEventListener('scroll', stickyBar);
